fc41db56495a194eeec538e83cd4ce28ba308b11
n-ndini7 <hp@RJs-PC>
2020-10-19 15:51:04 +0530

    [nandini] Added master - welcome message

---
+++ src/main/java/IndianStateCensus/StateCensusAnalyer.java
@@ -0,0 +1,10 @@
+package IndianStateCensus;
+
+//Master branch - welome message
+public class StateCensusAnalyer {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State Census Analyser program!");
+		System.out.println("=================================================");
+	}
+}
